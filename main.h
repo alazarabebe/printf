4cfcca24c4e6a6e517a1955c9cc76d0f60899f26
alazarabebe <alazarabebe9@gmail.com>
2023-03-28 11:41:09 -0700

    q1

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+
+#endif /* MAIN_H */
